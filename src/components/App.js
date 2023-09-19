e3d6334e4a94b81c8aac27627b35f68f1479c4e3
vishal pawar <pawarvishal29999@gmail.com>
2023-09-19 11:33:02 +0530

    submit-shop-list

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const App = () => {
+
+
+  const relativeList = [
+    { key: 'relativeListItem1', name: 'Aunt Jane' },
+    { key: 'relativeListItem2', name: 'Uncle Bob' },
+    { key: 'relativeListItem3', name: 'Cousin Alice' },
+    { key: 'relativeListItem4', name: 'Grandma Mary' },
+    // Add more relatives here
+  ];
+
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+               <h1>Relatives to Visit This Diwali:</h1>
+      <ol>
+          {relativeList.map((relative) => (
+            <li key={relative.key}>{relative.name}</li>
+          ))}
+      </ol>
     </div>
   )
 }
